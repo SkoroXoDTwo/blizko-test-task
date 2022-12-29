8f3309d56c7b4f92733ed781f71ef470c4a78801
Alexey Belkov <lexabelco2013@yandex.ru>
2022-12-30 00:36:14 +0500

    feat: сверстать и адаптировать calculate-effect

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,1 +1,6 @@
-  import './index.scss';
+import "./index.scss";
+
+const headerBlock = document.querySelector('.header');
+const pageBlock = document.querySelector('.page');
+
+pageBlock.style.paddingTop = `${headerBlock.offsetHeight}px`
