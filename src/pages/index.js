25577ef34b8ff2aa70dd4b52626f141de0bed022
Alexey Belkov <lexabelco2013@yandex.ru>
2022-12-30 02:02:08 +0500

    feat: сверстать card

--- src/pages/index.js
+++ src/pages/index.js
@@ -2,5 +2,21 @@ import "./index.scss";
 
 const headerBlock = document.querySelector('.header');
 const pageBlock = document.querySelector('.page');
+const moreBtns = document.querySelectorAll('.card__btn-more');
 
 pageBlock.style.paddingTop = `${headerBlock.offsetHeight}px`
+
+moreBtns.forEach((btn) => {
+  btn.addEventListener(('click'), (e) => {
+    const content = e.target.nextElementSibling;
+
+    if(content.style.maxHeight) {
+      e.target.textContent = 'Подробнее';
+      content.style.maxHeight = null;
+    }
+    else {
+      e.target.textContent = 'Свернуть';
+      content.style.maxHeight = content.scrollHeight + 'px';
+    }
+  })
+});
